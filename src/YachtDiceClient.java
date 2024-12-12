2c3650061478088f2464f6f87a40a33e348608e8
hwooch <146716186+hwooch@users.noreply.github.com>
2024-12-12 18:53:19 +0900

    클라이언트 코드 수정

--- src/YachtDiceClient.java
+++ src/YachtDiceClient.java
@@ -1,4 +1,6 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,11 +8,11 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 
 public class YachtDiceClient extends JFrame {
-
     private String serverAddress;
     private int serverPort;
     private JTextField t_userID;
@@ -43,6 +45,404 @@ public class YachtDiceClient extends JFrame {
     private JButton b_send;
 
     String roomTitle_copy = "";
+
+    // 위에는 클라이언트에 필요한 변수
+    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 밑에는 게임창에 필요한 변수
+
+    private Image backgroundImage = new ImageIcon(getClass().getResource("/resources/background.jpg")).getImage();
+    private Image scoreBoard = new ImageIcon(getClass().getResource("/resources/scoreBoard.jpeg")).getImage();
+    private Image sendButton = new ImageIcon(getClass().getResource("/resources/send_button.png")).getImage();
+
+    JButton giveUpButton = new JButton("Give Up");
+    JButton b_roll;
+    JTextArea textArea = new JTextArea(10, 20);
+    private JTextArea chatDisplay; // 채팅 메시지 표시 영역
+    private JTextField t_input_GAME;  // 메시지 입력 필드
+    private JButton b_send_GAME;    // 메시지 보내기 버튼
+
+    final static int DICE_SIZE = 5;
+    final static int MAX_DICE_NUM = 6;
+
+    int[] dices;
+    int[] counts;
+
+    Random rand;
+    String title;
+
+    public void GameGUI() {
+        dices = new int[DICE_SIZE];
+        counts = new int[MAX_DICE_NUM];
+
+        rand = new Random();
+
+        JFrame newFrame = new JFrame(roomTitle_copy);
+        newFrame.setSize(1400, 800);
+        newFrame.setLocationRelativeTo(null); // 화면 중앙에 위치
+        newFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // 닫기 버튼 클릭 시 창만 닫힘
+
+        // 레이어드 페인(JLayeredPane) 생성
+        JLayeredPane layeredPane = new JLayeredPane();
+        newFrame.add(layeredPane); // JLayeredPane을 프레임에 추가
+
+        // 배경 패널을 레이어 0에 추가
+        JPanel backgroundPanel = BackgroundPanel();
+        backgroundPanel.setBounds(0, 0, newFrame.getWidth(), newFrame.getHeight());
+        layeredPane.add(backgroundPanel, Integer.valueOf(0)); // 레이어 0에 배경 추가
+
+        // 점수 패널을 레이어 1에 추가
+        JPanel scorePanel = ScorePanel();
+        scorePanel.setBounds(0, 0, 300, 750); // 점수판 크기 설정
+        layeredPane.add(scorePanel, Integer.valueOf(1)); // 레이어 1에 점수판 추가
+
+        // 채팅 패널을 레이어 2에 추가
+        JPanel chatPanel = ChatPanel();
+        chatPanel.setBounds(1000, 0, 320, 760); // 채팅판 크기 설정
+        layeredPane.add(chatPanel, Integer.valueOf(2)); // 레이어 2에 채팅판 추가
+
+        // 주사위 패널을 레이어 10에 추가
+        JPanel dicePanel = AddDiceComponents();
+        dicePanel.setBounds(330, 0, 700, 750);
+        layeredPane.add(dicePanel, Integer.valueOf(10)); // 레이어 10에 주사위 패널 추가
+
+        AddDiceComponents();
+
+        newFrame.setVisible(true); // 창을 보이도록 설정
+        newFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                YachtDiceClient.this.setVisible(true); // 기존 창 다시 보이기
+                quit_room(roomTitle);
+                printDisplay(roomTitle + " 게임 방에서 퇴장하였습니다.");
+            }
+        });
+    }
+
+    private JPanel BackgroundPanel() {
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(backgroundImage, 300, 0, 1200, 750, this);
+            }
+        };
+        panel.setLayout(null);
+
+        return panel;
+    }
+
+    private JPanel ScorePanel() {
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(scoreBoard, 0, 0, 300, 750, this);
+            }
+        };
+        panel.setLayout(null);
+        return panel;
+    }
+
+    private JPanel ChatPanel() { // 채팅 패널
+        JPanel panel = new JPanel();
+        panel.setOpaque(false); // 패널을 투명하게 설정
+        panel.setLayout(null);
+        // JTextArea 추가하여 메시지 표시
+        textArea.setEditable(false);
+        //textArea.setOpaque(false);
+        Border border = BorderFactory.createLineBorder(Color.darkGray, 2);
+        textArea.setBorder(border);
+        textArea.setBounds(15, 10, 250, 625);
+        textArea.setFont(new Font("맑은 고딕", Font.PLAIN, 14)); // 폰트 설정
+        panel.add(textArea);
+
+        // 메시지 입력 필드
+        t_input = new JTextField();
+        //t_input.setOpaque(false);
+        t_input.setBorder(border);
+        t_input.setBounds(15, 640, 220, 30);
+        panel.add(t_input);
+
+        // 메시지 보내기 버튼
+        b_send = new JButton();
+        String sendImage = "/resources/send_button.png";
+
+        // 이미지 로드 및 크기 조정
+        ImageIcon originalIcon = new ImageIcon(getClass().getResource(sendImage));
+        Image scaledImage = originalIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        // 버튼에 스케일된 이미지 설정
+        b_send.setIcon(scaledIcon);
+
+        // 버튼 크기 및 위치 설정
+        b_send.setBounds(235, 640, 30, 30);
+        b_send.setContentAreaFilled(false); // 배경 제거
+
+        // 패널에 추가
+        panel.add(b_send);
+
+        // 엔터와 버튼 클릭으로 메시지 전송
+        b_send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = t_input.getText();
+                textArea.append(message + "\n");
+                t_input.setText("");
+            }
+        });
+
+        t_input.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = t_input.getText();
+                textArea.append(message + "\n");
+                t_input.setText("");
+            }
+        });
+
+        return panel;
+    }
+
+    private JPanel AddDiceComponents() {
+        JPanel jPanel = new JPanel();
+        JButton[] b_dices = new JButton[DICE_SIZE];
+        jPanel.setOpaque(false); // 패널을 투명하게 설정
+        jPanel.setLayout(null);
+        Arrays.fill(dices, 1);
+
+        b_dices[0] = createDiceButton("resources/dice" + 1 + ".png", 110, 210);
+        b_dices[1] = createDiceButton("resources/dice" + 2 + ".png", 200, 210);
+        b_dices[2] = createDiceButton("resources/dice" + 3 + ".png", 295, 210);
+        b_dices[3] = createDiceButton("resources/dice" + 4 + ".png", 390, 210);
+        b_dices[4] = createDiceButton("resources/dice" + 5 + ".png", 490, 210);
+
+        for (int i = 0; i < DICE_SIZE; i++) { //주사위 버튼 만들기
+            jPanel.add(b_dices[i]);
+        }
+
+        // 굴리기 버튼
+        b_roll = new JButton();
+        String rollImage = "/resources/roll_button.png";
+
+        // 이미지 로드 및 크기 조정
+        ImageIcon originalIcon = new ImageIcon(getClass().getResource(rollImage));
+        Image scaledImage = originalIcon.getImage().getScaledInstance(200, 110, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        b_roll.setIcon(scaledIcon);
+        b_roll.setContentAreaFilled(false); // 배경 제거
+        b_roll.setBorderPainted(false);
+        b_roll.setBounds(230, 400, 200, 110);
+        b_roll.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                b_roll.setEnabled(false);
+
+                playSound("/resources/dice_roll.wav");
+
+                // 3초동안 굴리기
+                long startTime = System.currentTimeMillis();
+                Thread rollThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        long endTime = System.currentTimeMillis();
+                        try {
+                            while ((endTime - startTime) / 1000F < 2) {
+                                // 최종 값 결정 (2초 후)
+
+                                for (int i = 0; i < DICE_SIZE; i++) { // 킵하지 않은게 있으면 랜덤값 생성 후 이미지 그림
+                                    if (!(boolean) b_dices[i].getClientProperty("isSaved")) {
+                                        dices[i] = rand.nextInt(6) + 1;
+                                        ImgService.updateImage(b_dices[i], "resources/dice" + dices[i] + ".png");
+                                    }
+                                }
+
+                                jPanel.repaint();
+                                jPanel.revalidate();
+
+                                Thread.sleep(60);
+
+                                endTime = System.currentTimeMillis();
+                            }
+                            b_roll.setEnabled(true);
+
+                            for (int i = 0; i < DICE_SIZE; i++) { // isSaved 값 출력
+                                System.out.println(dices[i] + " " + b_dices[i].getClientProperty("isSaved"));
+                            }
+
+                            System.out.println(available()); // 족보 출력
+
+                        } catch (InterruptedException e) {
+                            System.out.println("Threading Error: " + e);
+                        }
+                    }
+                });
+                rollThread.start();
+            }
+        });
+        jPanel.add(b_roll);
+
+        return jPanel;
+    }
+
+    private String available() {
+        countDices();
+        String results = "[";
+
+        boolean hasResults = false;
+
+        // 초이스 선택 X일 시 초이스도 포함
+        if (checkNo1()) {
+            results += "포커 ";
+            hasResults = true;
+        }
+        if (checkNo2()) {
+            results += "풀하우스 ";
+            hasResults = true;
+        }
+        if (checkNo3()) {
+            results += "스몰 스트레이트 ";
+            hasResults = true;
+        }
+        if (checkNo4()) {
+            results += "라지 스트레이트 ";
+            hasResults = true;
+        }
+        if (checkNo5()) {
+            results += "요트 ";
+            hasResults = true;
+        }
+
+        // 어디에도 해당 안되면 -> 1~6 숫자 각각 더하기 칸
+        if (!hasResults) {
+            results += "해당 없음";
+        }
+
+        results += "]";
+        return results;
+    }
+
+    private void countDices() { //굴린 주사위 수 저장
+        Arrays.fill(counts, 0); //count 0으로 초기화
+
+        for (int i = 0; i < DICE_SIZE; i++) {
+            counts[dices[i] - 1]++;
+        }
+    }
+
+    private boolean checkNo1() { //포커 (같은 숫자 4개)
+        boolean check = false;
+        for (int count : counts) {
+            if (count >= 4)
+                check = true;
+        }
+        return check;
+    }
+
+    private boolean checkNo2() { //풀하우스 체크
+        boolean check2 = false;
+        boolean check3 = false;
+        for (int count : counts) {
+            if (count == 2) {
+                check2 = true;
+            } else if (count == 3) {
+                check3 = true;
+            }
+        }
+        return check2 && check3;
+    }
+
+    private boolean checkNo3() { //스몰스트레이트
+        boolean check = false;
+        if (counts[2] >= 1 && counts[3] >= 1) { //스몰스트레이트 시 3,4 는 무조건 포함
+            if (counts[0] >= 1 && counts[1] >= 1 || counts[1] >= 1 && counts[4] >= 1 || counts[4] >= 1 && counts[5] >= 1) { // 1234 , 2345, 3456
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    private boolean checkNo4() { //라지스트레이트
+        boolean check = false;
+        if (counts[1] >= 1 && counts[2] >= 1 && counts[3] >= 1 && counts[4] >= 1) { //라지스트레이트 시 2,3,4,5는 무조건 포함
+            if (counts[0] >= 1 || counts[5] >= 1) {
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    private boolean checkNo5() { //요트 (같은 숫자 5개)
+        boolean check = false;
+        for (int count : counts) {
+            if (count == 5)
+                check = true;
+        }
+        return check;
+    }
+
+    private JButton createDiceButton(String imagePath, int x, int y) {
+        JButton b_dice = ImgService.loadImage(imagePath);
+        b_dice.setBounds(x, y, 75, 75);
+
+        // "isSaved" 상태를 버튼에 저장 (기본값은 false)
+        b_dice.putClientProperty("isSaved", false);
+
+        // 클릭 시 특정 위치로 이동하도록 이벤트 추가
+        b_dice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean isSaved = (boolean) b_dice.getClientProperty("isSaved");
+
+                if (!isSaved) { //false일 때
+                    // 이동
+                    b_dice.setLocation(b_dice.getX(), b_dice.getY() - 130); // 예시: 아래쪽으로 130px 이동
+                    b_dice.putClientProperty("isSaved", true);  // 이동 상태 업데이트
+                    repaint();
+                }
+                if (isSaved) {
+                    // 원래 위치로 돌아감
+                    b_dice.setLocation(b_dice.getX(), b_dice.getY() + 130);
+                    b_dice.putClientProperty("isSaved", false); // 이동 상태 업데이트
+                    repaint();
+                }
+                repaint();
+            }
+        });
+
+        return b_dice;
+    }
+
+    private void playSound(String soundFile) {
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(getClass().getResource(soundFile));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.err.println("Error playing sound: " + e.getMessage());
+        }
+    }
+
+    private void openRoomWindow(String roomTitle) {
+        this.setVisible(false);  // 클라이언트 창 숨기기
+        GameGUI();
+    }
+
+    // 주석 사이에는 GameGUI 코드
+
+    /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void send_message_room(String roomTitle) {
+        String roomtitle_temp = roomTitle;
+        send(new Yacht(t_userID.getText(), Yacht.MODE_TX_STRING_ROOM, roomtitle_temp, t_input_test.getText()));
+        t_input_test.setText("");
+    }
+
+    private void printDisplay2(String msg) {
+        t_display_test.append(msg + "\n");
+        t_display_test.setCaretPosition(t_display_test.getDocument().getLength());
+    }
 
     public YachtDiceClient(String serverAddress, int serverPort) {
         super("Yacht Game Client");
@@ -139,81 +539,9 @@ public class YachtDiceClient extends JFrame {
         }
     }
 
-//    private void openRoomWindow(String roomTitle) {
-//        // 방 접속 후 바로 GameGUI 창을 띄운다
-//        this.setVisible(false);  // 클라이언트 창 숨기기
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                GameGUI gameGUI = new GameGUI(YachtDiceClient.this, in, out);
-//
-//                gameGUI.addWindowListener(new WindowAdapter() {
-//                    @Override
-//                    public void windowClosed(WindowEvent e) {
-//                        quit_room(roomTitle);
-//                        printDisplay(roomTitle + " 게임 방에서 퇴장하였습니다.");
-//
-//                    }
-//                });
-//                gameGUI.setVisible(true);
-//            }
-//        });
-//    }
-
-    private void openRoomWindow(String roomTitle) {
-        this.setVisible(false);  // 클라이언트 창 숨기기
-        JFrame newFrame = new JFrame(roomTitle);
-        newFrame.setSize(700, 800);
-        newFrame.setLocationRelativeTo(null);
-        newFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // 닫기 버튼 클릭 시 창만 닫힘
-
-        t_display_test = new JTextArea();
-        t_display_test.setEditable(false);
-        newFrame.add(t_display_test, BorderLayout.NORTH);
-        t_input_test = new JTextField(20);
-        newFrame.add(t_input_test, BorderLayout.CENTER);
-        t_input_test.addActionListener(new ActionListener() { // 엔터 치면 입력한 글자가 보내짐
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                send_message_room(roomTitle);
-            }
-        });
-
-        // 창이 닫힐 때 기존 창을 다시 보이도록 설정
-        newFrame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                newFrame.dispose(); // 새로운 창 닫기
-                YachtDiceClient.this.setVisible(true); // 기존 창 다시 보이기
-                quit_room(roomTitle);
-                printDisplay(roomTitle + " 게임 방에서 퇴장하였습니다.");
-            }
-
-            @Override
-            public void windowClosed(WindowEvent e) {
-                YachtDiceClient.this.setVisible(true); // 기존 창 다시 보이기
-                quit_room(roomTitle);
-                printDisplay(roomTitle + " 게임 방에서 퇴장하였습니다.");
-            }
-        });
-
-        newFrame.setVisible(true);
-    }
-
-    private void printDisplay2(String msg) {
-        t_display_test.append(msg + "\n");
-        t_display_test.setCaretPosition(t_display_test.getDocument().getLength());
-    }
-
     private void quit_room(String roomTitle) {
         String roomtitle_temp = roomTitle;
         send(new Yacht(t_userID.getText(), Yacht.MODE_QUIT_ROOM, roomtitle_temp));
-    }
-
-    private void send_message_room(String roomTitle) {
-        String roomtitle_temp = roomTitle;
-        send(new Yacht(t_userID.getText(), Yacht.MODE_TX_STRING_ROOM, roomtitle_temp, t_input_test.getText()));
-        t_input_test.setText("");
     }
 
     private void connectToServer() throws UnknownHostException, IOException {
